f20edcdffb3aef3c0ca78e00edba4d31688f9c2e
Doug Liu <dougdliu@gmail.com>
2020-02-02 22:41:24 -0700

    1. Added Drivetrain Options

--- frc2020-scouting-web-app-client/src/app/pit-scouting-form/pit-scouting-form.component.ts
+++ frc2020-scouting-web-app-client/src/app/pit-scouting-form/pit-scouting-form.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-pit-scouting-form',
-  templateUrl: './pit-scouting-form.component.html',
-  styleUrls: ['./pit-scouting-form.component.css']
+  selector: "app-pit-scouting-form",
+  templateUrl: "./pit-scouting-form.component.html",
+  styleUrls: ["./pit-scouting-form.component.css"]
 })
 export class PitScoutingFormComponent implements OnInit {
+  driveTrains: string[] = [
+    "Tank",
+    "Mecanum",
+    "Slide / Holonomic",
+    "Butterfly / Grasshopper",
+    "Kiwi / Triangle",
+    "Nonadrive / Butterslide",
+    "Octocanum",
+    "Swerve"
+  ];
 
-  constructor() { }
+  gearboxes: string[] = [
+    "Single-Stage",
+    "Dual-Stage",
+    "Triple-Stage",
+    "Custom"
+  ];
 
-  ngOnInit() {
-  }
+  motors: string[] = [
+    "CIM",
+    "Mini CIM",
+    "BAG",
+    "Neo Brushless",
+    "Neo Brushless 550",
+    "775 Pro",
+    "775 Redline",
+    "Falcon 500"
+  ];
 
+  wheels: string[] = [
+    "HiGrip",
+    "SmoothGrip",
+    "Stealth",
+    "Mecanum",
+    "Performance",
+    "Omni",
+    "Pneumatic",
+    "Plaction"
+  ];
+
+  constructor() {}
+
+  ngOnInit() {}
 }
