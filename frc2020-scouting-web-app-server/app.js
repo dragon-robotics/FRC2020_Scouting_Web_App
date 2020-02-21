1c639d10ead6a7e0c976274465fb4ad049a042e4
Doug Liu <dougdliu@gmail.com>
2020-02-21 00:15:15 -0700

    1. Added TBA API routes for testing and information

--- frc2020-scouting-web-app-server/app.js
+++ frc2020-scouting-web-app-server/app.js
@@ -6,8 +6,16 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var api = require('./routes/api.route');
 
 var app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- frc2020-scouting-web-app-server/routes/api.route.js
+++ frc2020-scouting-web-app-server/routes/api.route.js
@@ -1,5 +1,177 @@
 var express = require('express');
 var Promise = require('bluebird');
 var router = express.Router();
-var routerPromise = require('request-promise');
+var requestPromise = require('request-promise');
 var _ = require('lodash');
+
+var scoutingData = require('./api/scoutingData.route');
+
+router.use('/scoutingData', scoutingData);
+
+router.get('/teamEventInfo/:eventID', function (req, res) {
+    var event = req.params.eventID;
+
+    requestPromise({
+        method: 'GET',
+        url: 'https://www.thebluealliance.com/api/v3/event/' + event + '/teams/simple',
+        headers: {
+            'X-TBA-Auth-Key': 'dS9knumpOPRZJkI1FvSCSYhdnIj9dk2mfpqPMb50JbCQc9roaG9Hl3oZKTRYYOe0',
+        },
+    })
+    .then(function (result) {
+        var teams = _.chain(JSON.parse(result))
+            .map(function (team) {
+                return team.team_number;
+            })
+            .sortBy()
+            .value();
+        res.send(teams);
+    });
+});
+
+router.get("/teamMatchEventInfo/:eventID/:teamID", function(req, res) {
+  var event = req.params.eventID;
+  var team = req.params.teamID;
+
+  requestPromise({
+    method: "GET",
+    url:
+      "https://www.thebluealliance.com/api/v3/team/" +
+      team +
+      "/event/" +
+      event +
+      "/matches/simple",
+    headers: {
+      "X-TBA-Auth-Key":
+        "dS9knumpOPRZJkI1FvSCSYhdnIj9dk2mfpqPMb50JbCQc9roaG9Hl3oZKTRYYOe0"
+    }
+  }).then(function(result) {
+    var order = {
+      Qual: 1,
+      "Elimination-Finals": 500,
+      "Quarter-Finals": 1000,
+      "Semi-Finals": 1500,
+      Finals: 2000,
+      Unknown: 2500
+    };
+    var matches = _.chain(JSON.parse(result))
+      .map(function(match) {
+        var comp_level = "";
+        switch (match.comp_level) {
+          case "qm":
+            comp_level = "Qual";
+            break;
+          case "ef":
+            comp_level = "Elimination-Finals";
+            break;
+          case "qf":
+            comp_level = "Quarter-Finals";
+            break;
+          case "sf":
+            comp_level = "Semi-Finals";
+            break;
+          case "f":
+            comp_level = "Finals";
+            break;
+          default:
+            comp_level = "Unknown";
+        }
+
+        return comp_level + " " + match.match_number;
+      })
+      .sortBy(function(unsortedMatches) {
+        return (
+          order[unsortedMatches.split(" ")[0]] + +unsortedMatches.split(" ")[1]
+        );
+      })
+      .value();
+    res.send(matches);
+  });
+});
+
+router.get("/getMatchAndTeamInfo/:eventID/", function(req, res) {
+  var event = req.params.eventID;
+
+  requestPromise({
+    method: "GET",
+    url:
+      "https://www.thebluealliance.com/api/v3/event/" +
+      event +
+      "/matches/simple",
+    headers: {
+      "X-TBA-Auth-Key":
+        "dS9knumpOPRZJkI1FvSCSYhdnIj9dk2mfpqPMb50JbCQc9roaG9Hl3oZKTRYYOe0"
+    }
+  }).then(function(result) {
+    var order = {
+      Qual: 1,
+      "Elimination-Finals": 500,
+      "Quarter-Finals": 1000,
+      "Semi-Finals": 1500,
+      Finals: 2000,
+      Unknown: 2500
+    };
+    var matches = _.chain(JSON.parse(result))
+      .map(function(match) {
+        var comp_level = "";
+        switch (match.comp_level) {
+          case "qm":
+            comp_level = "Qual";
+            break;
+          case "ef":
+            comp_level = "Elimination-Finals";
+            break;
+          case "qf":
+            comp_level = "Quarter-Finals";
+            break;
+          case "sf":
+            comp_level = "Semi-Finals";
+            break;
+          case "f":
+            comp_level = "Finals";
+            break;
+          default:
+            comp_level = "Unknown";
+        }
+
+        var matchObj = {};
+        var alliances = _.chain(match.alliances)
+          .mapValues(function(obj) {
+            return obj.team_keys;
+          })
+          .mapValues(function(obj) {
+            return _.map(obj, function(result) {
+              return _.replace(result, "frc", "");
+            });
+          })
+          .value();
+        if (comp_level == "Qual") {
+          matchObj[comp_level + " " + match.match_number] = {
+            matchKey: match.key,
+            alliances: _.chain(alliances)
+          };
+        } else {
+          matchObj[
+            comp_level + " " + match.set_number + " Match " + match.match_number
+          ] = {
+            matchKey: match.key,
+            alliances: _.chain(alliances)
+          };
+        }
+        // matchObj[alliances] = match.alliances;
+        return matchObj;
+        // return {key: match.key, human: comp_level+" "+match.match_number};
+      })
+      .sortBy(function(unsortedMatches) {
+        var match = _.head(_.keysIn(unsortedMatches));
+        return order[match.split(" ")[0]] + +match.split(" ")[1];
+      })
+      .reduce(function(result, value, key) {
+        return _.merge(result, value);
+      }, {})
+      .value();
+    res.send(matches);
+  });
+});
+
+module.exports = router;
